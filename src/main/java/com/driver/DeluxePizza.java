4553d2531412a1822cd5f7cc02ffe0235b0b8ca7
Your Name <Vijay>
2023-01-18 14:10:06 +0530

    Solution updated

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,7 +5,7 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        setCheeseAdded(true);
-        setToppingsAdded(true);
+//        setCheeseAdded(true);
+//        setToppingsAdded(true);
     }
 }
